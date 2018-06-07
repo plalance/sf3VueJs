c51aedd132c6eb1b8a0e0eaef38a777e2f4e5e7c
paul <paul@webetdesign.com>
2018-06-07 15:52:11 +0200

    ajout Model Sample -> Exemple de PUT / UDPATE / Listing sur un objet persisté

---
+++ src/SiteBundle/Form/SampleType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SiteBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SampleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('id', HiddenType::class, ['mapped' => true]);
+        $builder->add('label', HiddenType::class, ['mapped' => true]);
+        $builder->add('description', HiddenType::class, ['mapped' => true]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'SiteBundle\Entity\Sample',
+                'csrf_protection' => false,
+            ]
+        );
+    }
+}
