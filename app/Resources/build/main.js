ec394cddf7377b296024e9c02a4600a562efc5ae
paul <paul@courriel.exemple.fr>
2018-06-15 02:47:17 +0200

    Retrait de l'appel MarkerClusterer (supprimé du package.json)

--- app/Resources/build/main.js
+++ app/Resources/build/main.js
@@ -2,7 +2,6 @@ import $ from 'jquery';
 import uikit from 'uikit';
 import Axios from "axios";
 import icons from 'uikit/dist/js/uikit-icons';
-import cluster from 'js-marker-clusterer'
 
 console.log("main : resources/main.js");
 
