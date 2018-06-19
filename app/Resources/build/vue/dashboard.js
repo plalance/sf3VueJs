f65cdd413c025bde7732becf4248869c8ced4024
paul <lalance.paul@gmail.com>
2018-06-19 22:36:10 +0200

    fix js + ajout librairie materialize

---
+++ app/Resources/build/vue/dashboard.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import axios from "axios";
+import VueAxios from 'vue-axios';
+import VueLodash from 'vue-lodash';
+import Dashboard from './dashboard.vue';
+import VueQuillEditor from 'vue-quill-editor';
+
+window.Vue = Vue;
+
+Vue.use(VueAxios, axios, VueLodash, VueQuillEditor);
+
+let app = new Vue({
+    el: '#app',
+    data: datas,
+    render: h => h(Dashboard),
+});
