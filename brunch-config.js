6d7f3f76a7ba338d2d7fa7e55cbc076134e84421
paul <lalance.paul@gmail.com>
2018-06-19 20:12:04 +0200

    Configuration Brunch pour fichiers .js minifiés

--- brunch-config.js
+++ brunch-config.js
@@ -39,14 +39,21 @@ exports.config = {
             }
         },
         copycat:{
-            // "fonts" : ["bower_components/material-design-iconic-font", "bower_components/font-awesome/fonts"],
             "images": ["./app/Resources/build/images"],
             "js": ["./app/Resources/build/libraries", './node_modules/jquery/dist/jquery.slim.js'],
-            verbose : true, //shows each file that is copied to the destination directory
+            verbose : false, //shows each file that is copied to the destination directory
             onlyChanged: true //only copy a file if it's modified time has changed (only effective when using brunch watch)
         },
         postcss: {
             modules: true,
+        },
+        uglify: {
+            mangle: false,
+            compress: {
+                global_defs: {
+                    DEBUG: true
+                }
+            }
         },
         // Globales se déclarent ic ou dans les fichiers js avec import ... from '...'
         globals: {
