52253534b4db2ae8fa31dc83f0893578b5c6c9dd
paul <lalance.paul@gmail.com>
2018-06-20 13:49:15 +0200

    ajout lien documentation

--- app/Resources/build/vue/dashboard.vue
+++ app/Resources/build/vue/dashboard.vue
@@ -1,226 +1,104 @@
 <template>
-    <div class="uk-offcanvas-content" id="app">
+    <div id="app">
         <Loader :state="state"></Loader>
-        <h1 v-if="!user.id">Aucun User connecté</h1>
-        <div v-else="">
-            <h1>{{ user.username }}</h1>
-            <p>Email : {{ user.email }}</p>
-            <p>Roles : {{ user.roles }}</p>
-            <p>Activé : {{ user.enabled ? 'Activé' : 'Non Activé' }}</p>
-            <input type="text" v-model="notification_content">
-            <button @click="notifyAdmin()">Envoyer un mail de notification</button>
-            <button @click="logout()">Se déconnecter</button>
-            <div>
-                <h3>Sa présentation</h3>
-                <div v-html="user.presentation"></div>
+        <div class="row">
+            <h1 class="title" v-if="!user.id">Aucun User connecté</h1>
+            <div v-else="" class="row">
+                <div class="col S12 m6">
+                    <h1>{{ user.username }}</h1>
+                    <p>Email : {{ user.email }}</p>
+                    <p>Roles : {{ user.roles }}</p>
+                    <p>Activé : {{ user.enabled ? 'Activé' : 'Non Activé' }}</p>
+                    <input type="text" v-model="notification_content">
+                    <button @click="notifyAdmin()">Envoyer un mail de notification</button>
+                    <button @click="logout()">Se déconnecter</button>
+                </div>
+                <div class="col s12 m6">
+                    <div>
+                        <h3>Sa présentation</h3>
+                        <div v-html="user.presentation"></div>
+                    </div>
+                </div>
             </div>
-        </div>
-
-        <div>
-            <select id="super" name="super" v-model="userId" @change="usurpateUser()" class="material-select">
-                <option v-for="user in users" :value="user.id">Se connecter avec {{ user.username }}</option>
-            </select>
-            <label for="super">Se connecter en tant que :</label>
+            <div class="row">
+                <div class="col S12 m6">
+                    <select id="super" name="super" v-model="userId" @change="usurpateUser()" class="material-select">
+                        <option v-for="user in users" :value="user.id">Se connecter avec {{ user.username }}</option>
+                    </select>
+                    <label for="super">Se connecter en tant que :</label>
+                </div>
+            </div>
         </div>
         <!--<form class="uk-form">-->
-            <!--<fieldset data-uk-margin>-->
-                <!--<l>Se connecter en tant que :</l>-->
-                <!--<select v-model="userId" @change="usurpateUser()">-->
-                    <!--<option v-for="user in users" :value="user.id">Se connecter avec {{ user.username }}</option>-->
-                <!--</select>-->
-            <!--</fieldset>-->
+        <!--<fieldset data-uk-margin>-->
+        <!--<l>Se connecter en tant que :</l>-->
+        <!--<select v-model="userId" @change="usurpateUser()">-->
+        <!--<option v-for="user in users" :value="user.id">Se connecter avec {{ user.username }}</option>-->
+        <!--</select>-->
+        <!--</fieldset>-->
         <!--</form>-->
 
-        <h1>User connecté :</h1>
-        <div id="content" data-uk-height-viewport="expand: true">
-            <div class="uk-container uk-container-expand">
-                <div class="uk-grid uk-grid-medium uk-grid-match" data-uk-grid>
-                    <!-- panel -->
-                    <div class="uk-width-1-2@s uk-width-1-2@l">
-                        <div class="uk-card uk-card-default uk-card-small uk-card-hover">
-                            <div class="uk-card-header">
-                                <div class="uk-grid uk-grid-small">
-                                    <div class="uk-width-auto"><h4 class="uk-margin-remove-bottom">Editeur de texte</h4>
-                                    </div>
-                                    <div class="uk-width-expand uk-text-right">
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: move"></a>
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: cog"></a>
-                                        <a href="#" class="uk-icon-link" data-uk-icon="icon: close"></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="uk-card-body">
-                                <vue-quill-editor class="quill-editor" v-model="content"
-                                                  ref="myQuillEditor"
-                                                  :options="editorOption"
-                                                  @blur="onEditorBlur($event)"
-                                                  @focus="onEditorFocus($event)"
-                                                  @ready="onEditorReady($event)">
-                                </vue-quill-editor>
-                                <button @click="sendEmailContent()">Envoyer ce contenu par mail</button>
-                                <button @click="updatePresentation()">Définir comme nouvelle présentation</button>
-                                <button @click="updateSample()">Merge sample</button>
-                            </div>
-                        </div>
+        <h2 class="title">User connecté :</h2>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title">Editeur de texte</span>
+                        <vue-quill-editor class="quill-editor" v-model="content"
+                                          ref="myQuillEditor"
+                                          :options="editorOption"
+                                          @blur="onEditorBlur($event)"
+                                          @focus="onEditorFocus($event)"
+                                          @ready="onEditorReady($event)">
+                        </vue-quill-editor>
                     </div>
-                    <div class="uk-width-1-2@s uk-width-1-2@l">
-                        <div class="uk-card uk-card-default uk-card-small uk-card-hover">
-                            <div class="uk-card-header">
-                                <div class="uk-grid uk-grid-small">
-                                    <div class="uk-width-auto"><h4 class="uk-margin-remove-bottom">Geographie
-                                        (image)</h4>
-                                    </div>
-                                    <div class="uk-width-expand uk-text-right">
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: move"></a>
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: cog"></a>
-                                        <a href="#" class="uk-icon-link" data-uk-icon="icon: close"></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="uk-card-body">
-                                <img :src="staticFolder + 'images/mapa1.svg'" alt="">
-                                <p class="uk-text-muted uk-text-small uk-text-center">Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
+                    <div class="card-action">
+                        <button @click="sendEmailContent()">Envoyer ce contenu par mail</button>
+                        <button @click="updatePresentation()">Définir comme nouvelle présentation</button>
+                        <button @click="updateSample()">Merge sample</button>
                     </div>
                 </div>
-                <div class="uk-grid uk-grid-medium uk-grid-match" data-uk-grid>
+            </div>
 
-                    <!-- /panel -->
-                    <!-- panel -->
-                    <div class="uk-width-1-2@s uk-width-1-3@l uk-width-1-4@xl">
-                        <div class="uk-card uk-card-default uk-card-small uk-card-hover">
-                            <div class="uk-card-header">
-                                <div class="uk-grid uk-grid-small">
-                                    <div class="uk-width-auto"><h4 class="uk-margin-remove-bottom">ChartJS</h4></div>
-                                    <div class="uk-width-expand uk-text-right">
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: move"></a>
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: cog"></a>
-                                        <a href="#" class="uk-icon-link" data-uk-icon="icon: close"></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="uk-card-body">
-                                <textarea name="" id="" cols="30" rows="10" v-model="chartText"></textarea>
-                                <canvas id="myChart" width="400" height="400"></canvas>
-                            </div>
-                        </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img :src="staticFolder + 'images/mapa1.svg'" alt="">
+                        <span class="card-title">Géographie</span>
                     </div>
-                    <!-- /panel -->
-                    <!-- panel -->
-                    <div class="uk-width-1-2@s uk-width-2-3@l uk-width-1-4@xl">
-                        <div class="uk-card uk-card-default uk-card-small uk-card-hover">
-                            <div class="uk-card-header">
-                                <div class="uk-grid uk-grid-small">
-                                    <div class="uk-width-auto"><h4 class="uk-margin-remove-bottom">Localisation</h4>
-                                    </div>
-                                    <div class="uk-width-expand uk-text-right">
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: move"></a>
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: cog"></a>
-                                        <a href="#" class="uk-icon-link" data-uk-icon="icon: close"></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="uk-card-body">
-                                <Gmap :marker-coordinates="markerCoordinates" :name="'supermap'" :zoom="14"></Gmap>
-                            </div>
-                        </div>
+                    <div class="card-content">
+                        <p>Carte du monde</p>
+                    </div>
+                    <div class="card-action">
                     </div>
                 </div>
-                <div class="uk-grid uk-grid-medium uk-grid-match" data-uk-grid>
-                    <div class="uk-width-1-2@s uk-width-1-2@l uk-width-1-4@xl">
-                        <div class="uk-card uk-card-default uk-card-small uk-card-hover">
-                            <div class="uk-card-header">
-                                <div class="uk-grid uk-grid-small">
-                                    <div class="uk-width-auto"><h4 class="uk-margin-remove-bottom">Diagramme barre</h4>
-                                    </div>
-                                    <div class="uk-width-expand uk-text-right">
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: move"></a>
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: cog"></a>
-                                        <a href="#" class="uk-icon-link" data-uk-icon="icon: close"></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="uk-card-body">
-                                <img :src="staticFolder + 'images/mapa3.svg'" alt="">
-                                <p class="uk-text-muted uk-text-small uk-text-center">Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
+            </div>
+
+            <div class="col s12 m12">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title">Localisation</span>
+                        <Gmap :marker-coordinates="markerCoordinates" :name="'supermap'" :zoom="14"></Gmap>
                     </div>
-                    <!-- /panel -->
-                    <!-- panel -->
-                    <div class="uk-width-1-2@s uk-width-1-2@l uk-width-1-4@xl">
-                        <div class="uk-card uk-card-default uk-card-small uk-card-hover">
-                            <div class="uk-card-header">
-                                <div class="uk-grid uk-grid-small">
-                                    <div class="uk-width-auto"><h4 class="uk-margin-remove-bottom">Ipsum dolor</h4>
-                                    </div>
-                                    <div class="uk-width-expand uk-text-right">
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: move"></a>
-                                        <a href="#" class="uk-icon-link uk-margin-small-right"
-                                           data-uk-icon="icon: cog"></a>
-                                        <a href="#" class="uk-icon-link" data-uk-icon="icon: close"></a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="uk-card-body">
-                                <img :src="staticFolder + 'images/mapa4.svg'" alt="">
-                                <p class="uk-text-muted uk-text-small uk-text-center">Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
+                    <div class="card-action">
+                        <a href="/cartography">Voir la cartographie</a>
                     </div>
-                    <!-- /panel -->
                 </div>
             </div>
-        </div>
-        <!-- /CONTENT -->
-        <!-- OFFCANVAS -->
-        <div id="offcanvas-nav" data-uk-offcanvas="flip: true; overlay: true">
-            <div class="uk-offcanvas-bar uk-offcanvas-bar-animation uk-offcanvas-slide">
-                <button class="uk-offcanvas-close uk-close uk-icon" type="button" data-uk-close></button>
-                <ul class="uk-nav uk-nav-default">
-                    <li class="uk-active"><a href="#">Active</a></li>
-                    <li class="uk-parent">
-                        <a href="#">Parent</a>
-                        <ul class="uk-nav-sub">
-                            <li><a href="#">Sub item</a></li>
-                            <li><a href="#">Sub item</a></li>
-                        </ul>
-                    </li>
-                    <li class="uk-nav-header">Header</li>
-                    <li><a href="#js-options"><span class="uk-margin-small-right uk-icon"
-                                                    data-uk-icon="icon: table"></span>
-                        Item</a></li>
-                    <li><a href="#"><span class="uk-margin-small-right uk-icon" data-uk-icon="icon: thumbnails"></span>
-                        Item</a>
-                    </li>
-                    <li class="uk-nav-divider"></li>
-                    <li><a href="#"><span class="uk-margin-small-right uk-icon" data-uk-icon="icon: trash"></span> Item</a>
-                    </li>
-                </ul>
-                <h3>Title</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip
-                    ex ea commodo consequat.</p>
+
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title">ChartJs</span>
+                        <canvas id="myChart" width="400" height="400"></canvas>
+                    </div>
+                    <div class="card-action">
+                        <textarea name="" id="" cols="30" rows="10" v-model="chartText"></textarea>
+                    </div>
+                </div>
             </div>
+
         </div>
-        <!-- /OFFCANVAS -->
     </div>
 </template>
 
@@ -330,8 +208,8 @@
                 this.myDoughnutChart.update();
             },
             content: function () {
-                if (this.user.id){
-                    this.$set(this.user,'presentation', this.content)
+                if (this.user.id) {
+                    this.$set(this.user, 'presentation', this.content)
                 }
             },
             users: function () {
