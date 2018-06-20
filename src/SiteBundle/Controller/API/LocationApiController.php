1e361efa12d2843aec54e6e98cde09ecede17c67
paul <lalance.paul@gmail.com>
2018-06-20 12:03:01 +0200

    ajout template edition / list de locations + controleur location

---
+++ src/SiteBundle/Controller/API/LocationApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SiteBundle\Controller\API;
+
+use JMS\Serializer\SerializationContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use SiteBundle\Entity\Sample;
+use SiteBundle\Form\SampleType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Swagger\Annotations as SWG;
+
+class LocationApiController extends Controller
+{
+    /**
+     * Liste de toutes les localisations, avec leurs events associés
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns serialized DoctrineCollection of Locations",
+     * )
+     * @SWG\Tag(name="Localisations")
+     *
+     **/
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        // Get all locations
+        $locations = $this->getDoctrine()
+            ->getRepository('SiteBundle:Location')
+            ->findAll();
+        // Context application + serialisation
+        $ctx = SerializationContext::create()->setGroups(array('application'));
+        $ctx->setSerializeNull(true);
+        $serializer = $this->get('jms_serializer');
+        return $this->json($serializer->serialize($locations, "json", $ctx));
+    }
+}
