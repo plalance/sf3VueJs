132238103230ce73d29320de8c5420ad4a2d28fd
paul <paul@webetdesign.com>
2018-06-22 20:08:31 +0200

    ajout support map box + geolocalisation

--- app/Resources/build/cartography_leaflet.js
+++ app/Resources/build/cartography_leaflet.js
@@ -1,30 +1,60 @@
 var mymap, locations;
 
-mymap = L.map('map').setView([47.6425875, 6.844186600000057], 8);
+var iconUrl = globals.IMG_DIR + '/map/';
+
+var defaultIcon = L.Icon.extend({
+    options: {
+        iconUrl: iconUrl + 'home-marker.svg',
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        iconAnchor: [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -76]
+    }
+});
+
+var pos = {
+    lat: 47.6425875,
+    lng: 6.844186600000057
+};
+
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        pos.lat = position.coords.latitude;
+        pos.lng = position.coords.longitude;
+
+        console.log(pos);
+        // Add marker
+        var marker = L.marker([loc.latitude, loc.longitude],
+            {
+                icon: new defaultIcon({iconUrl: iconUrl + "default-marker.svg"})
+            }
+        );
+        var popupHere = L.popup();
+        popup.setContent("<p class='infowindow__title'>Vous êtes ici !</p>");
+        marker.bindPopup(popupHere);
+        marker.addTo(mymap);
+
+    }, function () {
+    });
+} else {
+    // Browser doesn't support Geolocation
+}
+
+mymap = L.map('map').setView([pos.lat, pos.lng], 12);
 const urlLocationsAPI = Routing.generate('api_location_list');
 
 // Définir emplacement des images de la librairie
 L.Icon.Default.imagePath = globals.LEAFLET_IMAGE_PATH;
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='+globals.MAPBOX_API_TOKEN, {
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var mapBoxUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + globals.MAPBOX_API_TOKEN;
+L.tileLayer(osmUrl, {
     // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 22,
     id: 'mapbox.streets',
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
-
-var iconUrl = globals.IMG_DIR+'/map/';
-
-var defaultIcon = L.Icon.extend({
-    options: {
-        iconUrl: iconUrl+'home-marker.svg',
-        iconSize:     [38, 95],
-        shadowSize:   [50, 64],
-        iconAnchor:   [22, 94],
-        shadowAnchor: [4, 62],
-        popupAnchor:  [-3, -76]
-    }
-});
 
 axios.get(urlLocationsAPI).then(function (response) {
     locations = JSON.parse(response.data);
@@ -51,8 +81,6 @@ axios.get(urlLocationsAPI).then(function (response) {
 });
 
 function generateInfoWindows(marker, loc) {
-    console.log(loc);
-
     var evtUrl = Routing.generate('one_event');
     // this refers to marker instance
     var div = document.createElement('div');
@@ -67,7 +95,7 @@ function generateInfoWindows(marker, loc) {
     for (var i = 0; i < loc.events.length; i++) {
         var li = document.createElement('li');
         var a = document.createElement('a');
-        a.setAttribute('href', evtUrl+'/'+loc.events[i].id);
+        a.setAttribute('href', evtUrl + '/' + loc.events[i].id);
         a.setAttribute('target', '_blank');
         li.appendChild(a);
         a.innerHTML = loc.events[i].name;
