06a323fd96ec97cfebad4a92d041fcd7627ef4d3
unknown <lalance.paul@gmail.com>
2019-01-26 11:03:45 +0100

    add dashboard.js

--- app/Resources/build/vue/dashboard.js
+++ app/Resources/build/vue/dashboard.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import axios from "axios";
 import VueAxios from 'vue-axios';
 import VueLodash from 'vue-lodash';
-import Dashboard from './Pages/dashboard.vue';
+import Dashboard from './Pages/Dashboard';
 import VueQuillEditor from 'vue-quill-editor';
 
 window.Vue = Vue;
